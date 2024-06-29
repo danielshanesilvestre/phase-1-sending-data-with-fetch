81afcc28458092841847e7917af7f54679ea5d66
Daniel <136662533+danielshanesilvestre@users.noreply.github.com>
2024-06-29 08:27:34 -0500

    Completed lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Add your code here
+
+function submitData(name, email) {
+    const configuration = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        })
+    };
+
+    return fetch("http://localhost:3000/users", configuration)
+        .then(response => response.json())
+        .then(data => {
+            let p = document.createElement("p");
+            p.textContent = data.id;
+            document.querySelector("body")
+                .append(p);
+        })
+        .catch(error => {
+            let p = document.createElement("p");
+            p.textContent = error.message;
+            document.querySelector("body")
+                .append(p);
+        });
+}
